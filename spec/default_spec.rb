2941efc4a7ea37cf3bbac897781fe6b8fdad0f3a
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-20 16:56:38 +0200

    Add ChefSpec tests

---
+++ spec/default_spec.rb
@@ -0,0 +1,27 @@
+require 'chefspec'
+
+describe 'The recipe ps2dev::default' do
+  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'ps2dev::default' }
+
+  it 'should execute apt-get update' do
+    chef_run.should execute_command 'apt-get update'
+  end
+
+  %w(gcc git-core make patch wget).each do |pkg|
+    it "should install #{pkg}" do
+      chef_run.should install_package pkg
+    end
+  end
+
+  it 'should execute toolchain-sudo.sh' do
+    chef_run.should execute_command './toolchain-sudo.sh'
+  end
+
+  it 'should delete directory /tmp/toolchain' do
+    chef_run.should delete_directory '/tmp/toolchain'
+  end
+
+  it 'should create cookbook file /etc/profile.d/ps2dev.sh' do
+    chef_run.should create_cookbook_file '/etc/profile.d/ps2dev.sh'
+  end
+end
