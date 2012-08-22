acabe676b6ca825ea56686108a0b40b637a84bb1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-08-22 21:42:57 +0200

    Make Chef Solo the default provisioner

---
+++ recipes/default.rb
@@ -0,0 +1,43 @@
+tmp_dir = "/tmp/toolchain"
+
+# Update Debian package index.
+# NOTE: Would be better to use the apt cookbook here.
+execute "apt-get-update" do
+  command "apt-get update"
+  ignore_failure true
+end
+
+# Install required Debian packages.
+%w(gcc git-core make patch wget).each do |pkg|
+  package(pkg) do
+    action :install
+  end
+end
+
+# Clone toolchain.
+git tmp_dir do
+  repository "git://github.com/ps2dev/ps2toolchain.git"
+  reference "master"
+  depth 1
+  action :sync
+end
+
+# Build and install toolchain.
+execute "toolchain-sudo.sh" do
+  user "root"
+  cwd tmp_dir
+  command "./toolchain-sudo.sh"
+  not_if "which ee-gcc iop-gcc ps2client && test -d /usr/local/ps2dev/ps2sdk"
+end
+
+# Clean up.
+directory tmp_dir do
+  recursive true
+  action :delete
+end
+
+# Set up environment variables.
+cookbook_file "/etc/profile.d/ps2dev.sh" do
+  mode 0644
+  action :create
+end
