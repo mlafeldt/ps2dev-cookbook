53088e073c0f9ccc0d2110fbe38228618a83a767
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2012-09-17 12:37:56 +0200

    Depend on git cookbook version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -7,10 +7,8 @@ long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
 recipe            "ps2dev::default", "Installs a full-fledged PS2 development environment"
 
-%w(ubuntu debian).each do |os|
-  supports os
-end
+supports "ubuntu"
+supports "debian"
 
-%w(build-essential git).each do |cb|
-  depends cb
-end
+depends "build-essential"
+depends "git", "1.0.0"
